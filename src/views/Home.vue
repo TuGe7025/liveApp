af01dc7bbcc2c6338054e7165e02be76782b65fd
TuGe7025 <1109320711@qq.com>
2020-02-18 15:17:17 +0800

    测试完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 class="title">一个弹幕视频demo</h2>
+    <d-player :options="options">
+    </d-player>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueDPlayer from 'vue-dplayer'
+import 'vue-dplayer/dist/vue-dplayer.css'
+
 
 export default {
-  name: 'Home',
+  data() {
+    return {
+      options: {
+        video: {
+          url: './voide/9979161.mp4',
+          // pic: '//i2.hdslb.com/bfs/archive/b76a7e12f4696757fc8f48f7f781cb85e61e4da4.jpg@560w_350h_100Q_1c.webp'
+        },
+        autoplay: false,
+        danmaku: {
+            id: '4157142',
+            api: 'http://127.0.0.1:1207/',
+            token: 'tokendemo',
+            user: 'TuGe7025',
+            bottom: '15%',
+            unlimited: true,
+        }
+      }
+    }
+  },
   components: {
-    HelloWorld
+    'd-player':VueDPlayer
   }
 }
 </script>
